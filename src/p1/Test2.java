a562286f2cd0d7deb279429b8deded2157718510
Yinghua <shenyinghua@bupt.edu.cn>
2021-10-12 20:45:10 +0800

    1.0.0

---
+++ src/p1/Test2.java
@@ -0,0 +1,11 @@
+package p1;
+/**
+ * @author Shen Yinghua
+ * @create 2021-10-12 20:42
+ */
+public class Test2 {
+    public void main(String[] args)
+    {
+        System.out.println("ABCDEFG");
+    }
+}
